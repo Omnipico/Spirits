c9304b6c537dfd7c389d7e834c07e306b896a7d9
HolyAntimony <maya@alyssa.ws>
2020-12-29 21:30:58 -0500

    Reincarnation locations

--- src/main/java/com/omnipico/spirits/Graveyard.java
+++ src/main/java/com/omnipico/spirits/Graveyard.java
@@ -1,10 +1,14 @@
 package com.omnipico.spirits;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import java.util.List;
 
@@ -53,12 +57,38 @@ public class Graveyard {
             setLives(player, this.config.getInt("non_canon_lives", 5));
             PotionEffect noInvisibility = new PotionEffect(PotionEffectType.INVISIBILITY, 0, 2, true, false, false);
             player.addPotionEffect(noInvisibility);
+            if (this.config.getBoolean("use_reincarnation_location", true)) {
+                List<World> worlds = Bukkit.getWorlds();
+                if (worlds.size() > 0) {
+                    World world = Bukkit.getWorlds().get(0);
+                    Location reincarnationLocation;
+                    if (this.config.getBoolean("reincarnate_at_spawn", true)) {
+                        reincarnationLocation = world.getSpawnLocation().clone();
+                        reincarnationLocation.add(0.5,0, 0.5);
+                    } else {
+                        reincarnationLocation = new Location(world,
+                                this.config.getDouble("reincarnation_x", 0),
+                                this.config.getDouble("reincarnation_y", 64),
+                                this.config.getDouble("reincarnation_z", 0)
+                                );
+                    }
+                    player.teleport(reincarnationLocation);
+                }
+            }
         } else if (!alive && isAlive(player)) {
             List<String> spirits = this.config.getStringList("spirits");
             spirits.add(player.getUniqueId().toString());
             this.config.set("spirits", spirits);
             plugin.saveConfig();
             setLives(player, 0);
+            BukkitScheduler scheduler = plugin.getServer().getScheduler();
+            scheduler.scheduleSyncDelayedTask(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    PotionEffect invisibility = new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1, true, false, false);
+                    player.addPotionEffect(invisibility);
+                }
+            }, 1L);
         }
     }
 
